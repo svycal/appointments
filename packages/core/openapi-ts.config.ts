678ce1cdadb99a105590ca3ab7e7ce5f5a88b78f
Derrick Reimer <derrickreimer@gmail.com>
2025-11-24 15:44:20 -0600

    Switch to openapi-typescript

--- packages/core/openapi-ts.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@hey-api/openapi-ts';
-
-export default defineConfig({
-  input: 'https://api.savvycal.app/v1/spec',
-  output: { format: 'prettier', lint: 'eslint', path: './src/client' },
-  plugins: ['@hey-api/client-fetch', '@tanstack/react-query'],
-});
---
+++ packages/core/src/index.ts
@@ -0,0 +1,14 @@
+import createFetchClient, { ClientOptions } from 'openapi-fetch';
+import type { paths } from './schema';
+
+export type { paths };
+
+/**
+ * Creates a client for the SavvyCal Appointments API.
+ */
+export const createClient = (options: ClientOptions) => {
+  return createFetchClient<paths>({
+    baseUrl: 'https://api.savvycal.app/v1',
+    ...options,
+  });
+};
