4c145582b12ac5e13b4d0353bea7e3d8ee244dfd
Derrick Reimer <derrickreimer@gmail.com>
2025-11-26 15:58:09 -0600

    Improvements to createClient

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,14 +1,58 @@
-import createFetchClient, { ClientOptions } from 'openapi-fetch';
+import createFetchClient, {
+  ClientOptions as OpenApiClientOptions,
+} from 'openapi-fetch';
 import type { paths } from './schema';
 
 export type { paths };
 
+const DEFAULT_BASE_URL = 'https://api.savvycal.app';
+
+export interface ClientOptions extends Omit<OpenApiClientOptions, 'baseUrl'> {
+  /**
+   * Base URL for the SavvyCal API.
+   * Defaults to 'https://api.savvycal.app'
+   */
+  baseUrl: string;
+
+  /**
+   * API key for authentication.
+   * If provided, will be used to set the Authorization header.
+   */
+  apiKey: string;
+
+  /**
+   * ID for the SavvyCal account.
+   * Used to scope requests to a specific account via the `X-SavvyCal-Account` header.
+   */
+  account: string;
+}
+
 /**
  * Creates a client for the SavvyCal Appointments API.
+ *
+ * @param options - Configuration options for the client
+ * @returns A configured API client.
  */
-export const createClient = (options: ClientOptions) => {
-  return createFetchClient<paths>({
-    baseUrl: 'https://api.savvycal.app',
+export const createClient = (options: Partial<ClientOptions> = {}) => {
+  const clientOptions = {
+    baseUrl: options.baseUrl ?? DEFAULT_BASE_URL,
+    headers: {},
     ...options,
-  });
+  };
+
+  if (options.apiKey) {
+    clientOptions.headers = {
+      ...clientOptions.headers,
+      Authorization: `Bearer ${options.apiKey}`,
+    };
+  }
+
+  if (options.account) {
+    clientOptions.headers = {
+      ...clientOptions.headers,
+      'X-SavvyCal-Account': options.account,
+    };
+  }
+
+  return createFetchClient<paths>(clientOptions);
 };
