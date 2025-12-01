0403fb7cf2e4a2d1aef5b38b49a942e81fce1249
Derrick Reimer <derrickreimer@gmail.com>
2025-12-01 15:47:39 -0600

    Dependencies

--- playgrounds/react/config/config.exs
+++ playgrounds/react/config/config.exs
@@ -36,7 +36,7 @@ config :esbuild,
   version: "0.25.4",
   demo: [
     args:
-      ~w(js/app.tsx --bundle --chunk-names=chunks/[name]-[hash] --splitting --format=esm --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
+      ~w(js/app.tsx --bundle --chunk-names=chunks/[name]-[hash] --splitting --format=esm --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
   ],
