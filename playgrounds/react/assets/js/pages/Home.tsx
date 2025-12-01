64411804777d119b65172c3bef257ee224355a59
Derrick Reimer <derrickreimer@gmail.com>
2025-12-01 14:09:03 -0600

    Add react day picker to playground

--- playgrounds/react/assets/js/pages/Home.tsx
+++ playgrounds/react/assets/js/pages/Home.tsx
@@ -2,9 +2,12 @@ import React, { ReactNode, useState } from "react";
 import { DateTime } from "luxon";
 import { RootLayout } from "../layouts/root-layout";
 import { usePublicServiceSlots } from "@savvycal/appointments-react-query";
+import { DayPicker } from "react-day-picker";
 
 const Home = () => {
   const [from, setFrom] = useState(DateTime.now().startOf("month"));
+
+  const [selected, setSelected] = useState<Date>();
 
   const { data, isLoading } = usePublicServiceSlots("srv_28f3a4bd5986", {
     from: from.toISODate(),
@@ -13,6 +16,17 @@ const Home = () => {
 
   return (
     <div>
+      <DayPicker
+        animate
+        mode="single"
+        selected={selected}
+        onSelect={setSelected}
+        footer={
+          selected
+            ? `Selected: ${selected.toLocaleDateString()}`
+            : "Pick a day."
+        }
+      />
       <button
         className="bg-zinc-400 p-3 rounded"
         onClick={() => {
