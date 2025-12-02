23a575812ccf7e82e073dc8b456e93cca0310f33
Derrick Reimer <derrickreimer@gmail.com>
2025-12-02 10:16:52 -0600

    Time zone handling

--- playgrounds/react/assets/js/pages/Home.tsx
+++ playgrounds/react/assets/js/pages/Home.tsx
@@ -1,48 +1,54 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useState, useEffect } from "react";
 import { RootLayout } from "../layouts/root-layout";
 import { usePublicServiceSlots } from "@savvycal/appointments-react-query";
 import { DayPicker } from "react-day-picker";
-import { formatISO, endOfMonth, startOfMonth, sub, add } from "date-fns";
+import {
+  formatISO,
+  endOfMonth,
+  startOfMonth,
+  fromUnixTime,
+  isSameDay,
+} from "date-fns";
+import { tz } from "@date-fns/tz";
 
 const Home = () => {
-  const [from, setFrom] = useState(startOfMonth(new Date()));
+  const [timeZone, setTimeZone] = useState<string>();
 
+  // Infer the browser's time zone
+  useEffect(() => {
+    setTimeZone(new Intl.DateTimeFormat().resolvedOptions().timeZone);
+  }, []);
+
+  const [month, setMonth] = useState<Date>(startOfMonth(new Date()));
   const [selected, setSelected] = useState<Date>();
 
   const { data, isLoading } = usePublicServiceSlots("srv_28f3a4bd5986", {
-    from: formatISO(from, { representation: "date" }),
-    until: formatISO(endOfMonth(from), { representation: "date" }),
+    from: formatISO(month, { representation: "date" }),
+    until: formatISO(endOfMonth(month), { representation: "date" }),
   });
+
+  const dateHasSlots = (date: Date) => {
+    if (!data || !timeZone) return false;
+
+    return data.data.some((slot) => {
+      const slotDate = fromUnixTime(slot.start_at_ts!);
+
+      return isSameDay(slotDate, date, { in: tz(timeZone) });
+    });
+  };
 
   return (
     <div>
       <DayPicker
         animate
+        disabled={(date) => !dateHasSlots(date)}
         mode="single"
         selected={selected}
         onSelect={setSelected}
-        footer={
-          selected
-            ? `Selected: ${selected.toLocaleDateString()}`
-            : "Pick a day."
-        }
+        month={month}
+        onMonthChange={setMonth}
       />
-      <button
-        className="bg-zinc-400 p-3 rounded"
-        onClick={() => {
-          setFrom(sub(from, { months: 1 }));
-        }}
-      >
-        Previous Month
-      </button>
-      <button
-        className="bg-zinc-400 p-3 rounded"
-        onClick={() => {
-          setFrom(add(from, { months: 1 }));
-        }}
-      >
-        Next Month
-      </button>
+
       {isLoading && <p>Loading...</p>}
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
