ad17d5d713deeb9e4ac5edb90c782fd704b0a491
Derrick Reimer <derrickreimer@gmail.com>
2025-12-11 22:29:29 -0600

    Accept an undefined response from fetchAccessToken

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -64,7 +64,7 @@ export type FetchClientOptions = {
 } & (
   | {
       demo?: never;
-      fetchAccessToken?: () => Promise<string> | string;
+      fetchAccessToken?: () => Promise<string | undefined> | string | undefined;
     }
   | {
       demo?: string;
